d388059a45632156eefc9226b5d514e639c8539d
maybe-adamant <maybe.adamant@gmail.com>
2026-05-13 00:08:12 -0400

    Adopt current module host activation

--- src/behaviors/SufferingFix.lua
+++ src/behaviors/SufferingFix.lua
@@ -13,7 +13,7 @@ table.insert(option_fns,
     })
 
 table.insert(hook_fns, function()
-    lib.hooks.Wrap(internal, "CheckSpawnCurseDamage", function(baseFunc, enemy, traitArgs)
+    lib.hooks.Wrap("CheckSpawnCurseDamage", function(baseFunc, enemy, traitArgs)
         if not internal.store.read("SufferingFix") or not lib.isModuleEnabled(internal.store, internal.PACK_ID) then
             return baseFunc(enemy, traitArgs)
         end
